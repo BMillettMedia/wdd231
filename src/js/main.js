6be71f15ae462efacf61b6b71e6403e93156ab4d
bmillettmedia <bryanmillett@cox.net>
2025-03-08 08:06:49 -0700

    NPS-6_first attempt

--- src/js/main.js
+++ src/js/main.js
@@ -33,23 +33,30 @@ function enableNavigation() {
   );
   // when the main menu button is clicked:
   menuButton.addEventListener("click", (ev) => {
-    let target = ev.target;
     // toggle the show class on the global-nav
     document.querySelector(".global-nav").classList.toggle("show");
-    // check to see if ev.target is the button or something inside the button
-    if (target.tagName != "BUTTON") {
-      target = target.closest("button");
-    }
     // check to see if we just opened or closed the menu
     if (document.querySelector(".global-nav").classList.contains("show")) {
       // if we opened it then set the aria-expanded attribute to true
-      target.setAttribute("aria-expanded", true);
+      menuButton.setAttribute("aria-expanded", true);
     } else {
       // if we closed it then set the aria-expanded attribute to false
-      target.setAttribute("aria-expanded", false);
+      menuButton.setAttribute("aria-expanded", false);
     }
 
     console.log("toggle");
+  });
+  subMenuToggles.forEach((toggle) => {
+    //for each submenu toggle
+    toggle.addEventListener("click", (ev) => {
+      // find the closest li ancestor, then find the submenu inside of that li and toggle the show class
+      ev.currentTarget
+        .closest("li")
+        .querySelector(".global-nav__submenu")
+        .classList.toggle("show");
+      // toggle the rotate class on the button icon that was clicked
+      ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
+    });
   });
 }
 
